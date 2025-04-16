f87b23c47439346619e96c8fdfbf43be3dfefbf9
K B <kalyanbtl@gmail.com>
2025-04-16 17:11:14 -0400

    crate for msp430fr5994

---
+++ target/debug/deps/critical_section-dc6790c673f1eac9.d
@@ -0,0 +1,9 @@
+/Users/kb/Documents/Generate PAC files/msp430fr5994/target/debug/deps/libcritical_section-dc6790c673f1eac9.rmeta: /Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/critical-section-1.2.0/src/lib.rs /Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/critical-section-1.2.0/src/mutex.rs /Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/critical-section-1.2.0/src/../README.md
+
+/Users/kb/Documents/Generate PAC files/msp430fr5994/target/debug/deps/libcritical_section-dc6790c673f1eac9.rlib: /Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/critical-section-1.2.0/src/lib.rs /Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/critical-section-1.2.0/src/mutex.rs /Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/critical-section-1.2.0/src/../README.md
+
+/Users/kb/Documents/Generate PAC files/msp430fr5994/target/debug/deps/critical_section-dc6790c673f1eac9.d: /Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/critical-section-1.2.0/src/lib.rs /Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/critical-section-1.2.0/src/mutex.rs /Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/critical-section-1.2.0/src/../README.md
+
+/Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/critical-section-1.2.0/src/lib.rs:
+/Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/critical-section-1.2.0/src/mutex.rs:
+/Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/critical-section-1.2.0/src/../README.md:
---
+++ target/debug/deps/vcell-252f5592d12385b8.d
@@ -0,0 +1,7 @@
+/Users/kb/Documents/Generate PAC files/msp430fr5994/target/debug/deps/libvcell-252f5592d12385b8.rmeta: /Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/vcell-0.1.3/src/lib.rs
+
+/Users/kb/Documents/Generate PAC files/msp430fr5994/target/debug/deps/libvcell-252f5592d12385b8.rlib: /Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/vcell-0.1.3/src/lib.rs
+
+/Users/kb/Documents/Generate PAC files/msp430fr5994/target/debug/deps/vcell-252f5592d12385b8.d: /Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/vcell-0.1.3/src/lib.rs
+
+/Users/kb/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/vcell-0.1.3/src/lib.rs:
